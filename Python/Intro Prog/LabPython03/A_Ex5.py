2424ea91a5022b546027487409a2853442ef641f
GiordanoMassimo <massimo.giord@gmail.com>
2023-10-21 13:23:42 +0200

    Nuovo Esercizio

--- Python/Intro Prog/LabPython03/A_Ex5.py
+++ Python/Intro Prog/LabPython03/A_Ex5.py
@@ -1,14 +1,27 @@
 def A_Ex5(s):
-    """MODIFICARE IL CONTENUTO DI QUESTA FUNZIONE PER SVOLGERE L'ESERCIZIO"""
-            
+    if s != '':
+        x = s.count('-') #Conto quante volte il carattere si ripete
+        list = [] #Creo una lista vuota
+        stringa = s
+        for i in range(x+1):
+            t = stringa.find('-')
+            print("i = ", i)
+            print("t = ", t)
+            print("stringa = ", stringa)
+            if t == -1:
+                list.append(stringa)
+            else:
+                list.append(stringa[:t])
+                stringa = stringa[t+1:]
+        max = 0
+        for i in range(len(list)):
+            if int(list[i]) >= max:
+                max = int(list[i])
+        return max
 
-###############################################################################
-
-"""NON MODIFICARE IL SEGUENTE CODICE (codice di test della funzione)"""
 
 if __name__ == '__main__':
     from tester import tester_fun
-    """SE NON VOLETE ESEGUIRE UN TEST COMMENTATE LA RIGA RELATIVA"""
 
     counter_test_positivi = 0
     total_tests = 5
@@ -20,6 +33,3 @@ if __name__ == '__main__':
     counter_test_positivi += tester_fun(A_Ex5, ['1-2-3-4-5'] ,5)
 
     print('La funzione',A_Ex5.__name__,'ha superato',counter_test_positivi,'test su',total_tests)
-
-
-
