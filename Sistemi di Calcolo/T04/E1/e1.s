654bf1fd02a5ff8c352a041d84f2f48c5c0075ec
MassimoGiordano03 <massimo.giord@gmail.com>
2024-03-22 13:06:27 +0000

    Fine Relazione 4

--- Sistemi di Calcolo/T04/E1/e1.s
+++ Sistemi di Calcolo/T04/E1/e1.s
@@ -1,10 +1,10 @@
 .globl count
 
-count:                                       # int count(const char *s1){
-    xorl %eax, %eax                          # int a=0;
-    movl 4(%esp), %ecx                       # const char* c = s1;
-A:  cmpl $0, %ecx                            # if(!*c) goto E;
-    jne E
+count:                                       # int count(const char *s1)
+    xorl %eax, %eax  # int a=0;
+    movl 4(%esp), %ecx  # const char* c = s1;
+A:  cmpb $0, (%ecx) # if(!*c) goto E;
+    je E
     incl %eax                                # a++;
     incl %ecx                                # c++;
     jmp A                                    # goto A;
--- Sistemi di Calcolo/T04/E3/e3.s
+++ Sistemi di Calcolo/T04/E3/e3.s
@@ -1,1 +1,19 @@
-// scrivere la soluzione qui...
+# scrivere la soluzione qui...
+.globl my_strcmp 
+
+my_strcmp:
+	movl 4(%esp), %ecx
+	movl 8(%esp), %edx
+L:
+	movb (%ecx), %al
+	cmpb $0, %al
+	je E
+	cmpb %al, (%edx)
+	jne E
+	incl %ecx
+	incl %edx
+	jmp L
+E:
+	movb (%ecx), %al
+	subb (%edx), %al
+	ret
