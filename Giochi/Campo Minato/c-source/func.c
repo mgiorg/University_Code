aedf5a732c250dcbff8a95db4fa40c342375ca9b
GiordanoMassimo <massimo.giord@gmail.com>
2024-04-05 15:31:19 +0200

    kjn

--- Giochi/Campo Minato/c-source/func.c
+++ Giochi/Campo Minato/c-source/func.c
@@ -10,8 +10,10 @@
 typedef struct
 {
 	int n, mine;
+	int* array;
 	int** campo_minato;
 	int** marcata;
+	int** risultato;
 } CampoMinato;
 
 CampoMinato* initBoard(int n, int mine) {
@@ -19,15 +21,19 @@ CampoMinato* initBoard(int n, int mine) {
 	CampoMinato* campo;
 	campo-> n = n;
 	campo-> mine = mine;
+	campo-> array = (int* )calloc(n*n, sizeof(int));
 	campo-> campo_minato = (int** )malloc(n*sizeof(int*));
 	campo-> marcata = (int** )malloc(n*sizeof(int*));
-	
+	campo-> risultato = (int **)malloc(n * sizeof(int *));
+
 	for(int i = 0; i < campo-> n; ++i) {
 		campo->campo_minato[i] = (int* )malloc(n * sizeof(int));
 		campo->marcata[i] = (int* )malloc(n * sizeof(int));
+		campo->risultato[i] = (int *)malloc(n * sizeof(int));
 		for(int j = 0; j < campo-> n; ++j) {
 			campo->campo_minato[i][j] = false;
 			campo->marcata[i][j] = false;
+			campo-> risultato[i][j] = -1;
 		}
 	}
 	return campo;
@@ -73,10 +79,25 @@ void print_board(CampoMinato *campo, char c)
 			printf("\n");
 		}
 	}
+	if (c == 'R') {
+		printf("-----Risultato-----\n");
+		for (int i = 0; i < campo->n; ++i) {
+			for (int j = 0; j < campo->n; ++j) {
+				if (campo->risultato[i][j] == -1)
+					printf("* ");
+				else if (campo->risultato[i][j] == false)
+					printf("  ");
+				else
+					printf("%d ", campo->risultato[i][j]);
+			}
+			printf("\n");
+		}
+	}
 }
 
 bool is_bomb(CampoMinato *campo, int row, int col) {
-	if(campo-> campo_minato[row][col] == 1) {
+	if (row >= 0 && col >= 0 && row < campo->n && col < campo->n && campo->campo_minato[row][col] == 1)
+	{
 		return true;
 	}
 	else return false;
@@ -98,32 +119,66 @@ bool completato(CampoMinato *campo) {
 }
 
 int percorribile(CampoMinato *campo, int row, int col) {
-	if (row < 0 || col < 0 || row >= campo-> n ||col >= campo->n) 
-		return false;
-	if(is_bomb(campo, row, col))
-		return false;
-	else return 1;
+	if (row >= 0 && col >= 0 && row < campo-> n &&col < campo->n && is_bomb(campo, row, col) == false) 
+		return true;
+	else return false;
 }
 
 int cercaVuoti(CampoMinato *campo, int row, int col) {
+	// * Deve sbloccare completamente solo le caselle che non
+	// * hanno bombe intorno. 
 	
 	if (completato(campo) == true)
 	{
 		return true;
 	}
-	if(percorribile(campo, row, col) == false) {
+	else if(percorribile(campo, row, col) == false) {
 		return false;
 	}
 	else {
-		if (campo->marcata[row][col] == 0)
+		if (campo->marcata[row][col] == false)
 		{
-			
-			campo-> marcata[row][col] = 1;
-			return  cercaVuoti(campo, row - 1, col) ||
-					cercaVuoti(campo, row + 1, col) ||
-					cercaVuoti(campo, row, col - 1) ||
-					cercaVuoti(campo, row, col + 1);
+			campo->marcata[row][col] = true;
+			if ((is_bomb(campo, row - 1, col - 1) || is_bomb(campo, row - 1, col) || is_bomb(campo, row - 1, col + 1) ||
+				is_bomb(campo, row, col - 1) 	 || is_bomb(campo, row, col + 1) ||
+				is_bomb(campo, row + 1, col - 1) || is_bomb(campo, row + 1, col) || is_bomb(campo, row + 1, col + 1)) == false)
+					return cercaVuoti(campo, row-1, col-1) ||
+						cercaVuoti(campo, row, col-1) ||
+						cercaVuoti(campo, row+1, col-1) ||
+						cercaVuoti(campo, row-1, col) ||
+						cercaVuoti(campo, row+1, col) ||
+						cercaVuoti(campo, row-1, col+1) ||
+						cercaVuoti(campo, row, col+1) ||
+						cercaVuoti(campo, row+1, col+1);
+					
 		}
-	}
+	
 	return false;
+	}
+}
+
+bool cellaCorretta(CampoMinato* campo, int row, int col) {
+	if (row >= 0 && col >= 0 && row < campo->n && col < campo->n)
+		return true;
+	else return false;
+}
+
+int contaBombe(CampoMinato* campo, int row, int col) {
+
+	if (is_bomb(campo, row, col) && campo-> marcata[row][col] == false) 
+		return 1;
+	else{
+		if(campo-> risultato[row][col] == -1 && campo-> marcata[row][col] == true) {
+			campo->risultato[row][col] = contaBombe(campo, row + 1, col - 1) +
+											contaBombe(campo, row, col - 1) +
+											contaBombe(campo, row - 1, col - 1) +
+											contaBombe(campo, row + 1, col) +
+											contaBombe(campo, row - 1, col) +
+											contaBombe(campo, row + 1, col + 1) +
+											contaBombe(campo, row, col + 1) +
+											contaBombe(campo, row - 1, col + 1);
+		}
+		return 0;
+	}
+	
 }
