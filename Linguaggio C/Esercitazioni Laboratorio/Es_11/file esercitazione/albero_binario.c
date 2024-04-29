5564cc502da917f820df4f03b9e9fd56bb3f23f1
GiordanoMassimo <massimo.giord@gmail.com>
2024-04-29 19:56:42 +0200

    kugf

---
+++ Linguaggio C/Esercitazioni Laboratorio/Es_11/file esercitazione/albero_binario.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "albero_binario.h"
+
+
+// Valore di errore
+TipoInfoAlbero ERRORE_InfoAlbBin='*';
+
+
+// Implementazioni del tipo astratto
+
+TipoAlbero albBinVuoto() {
+  return NULL;
+}
+
+TipoAlbero creaAlbBin(TipoInfoAlbero info, TipoAlbero sx, TipoAlbero dx) {
+  TipoAlbero a = (TipoAlbero) malloc(sizeof(TipoNodoAlbero));
+  a->info = info;
+  a->sinistro = sx;
+  a->destro = dx;
+  return a;
+}
+
+
+bool estVuoto(TipoAlbero a) {
+  return (a == NULL);
+}
+
+TipoInfoAlbero radice(TipoAlbero a) {
+  if (a == NULL) {
+    printf ("ERRORE accesso albero vuoto \n");
+    return ERRORE_InfoAlbBin;
+  } else {
+    return a->info;
+  }
+}
+
+
+TipoAlbero sinistro(TipoAlbero a) {
+  if (a == NULL) {
+    printf ("ERRORE accesso albero vuoto \n");
+    return NULL;
+  } else {
+    return a->sinistro;
+  }
+}
+
+TipoAlbero destro(TipoAlbero a) {
+  if (a == NULL) {
+    printf ("ERRORE accesso albero vuoto \n");
+    return NULL;
+  } else {
+    return a->destro;
+  }
+}
+
+
+void assegnaRadice(TipoAlbero a, TipoInfoAlbero info) {
+	if (a == NULL) {
+    printf ("ERRORE accesso albero vuoto \n");
+    return;
+	}
+	a->info = info;
+}
+
+
+// Funzioni esterne
+
+void stampaParentetica(TipoAlbero a) {
+    if (estVuoto(a)) {
+        printf("()");
+    }
+    else {
+        printf("( %c ", radice(a));
+        stampaParentetica(sinistro(a));
+        printf(" ");
+        stampaParentetica(destro(a));
+        printf(" )");
+    }
+}
+
+
